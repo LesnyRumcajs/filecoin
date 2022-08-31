30621a677ba2c1d99e1ea70fbe0f6347e105b743
Alex Su <alexanderytsu@gmail.com>
2022-08-31 14:54:48 +1000

    wip

--- testing/actor_factory/actors/factory_actor/src/bin/decode.rs
+++ testing/actor_factory/actors/factory_actor/src/bin/decode.rs
@@ -1,14 +1,31 @@
 use base64;
 use factory_actor::CreateFrc42Return;
 use fvm_ipld_encoding::RawBytes;
+use fvm_shared::bigint::bigint_ser;
+use fvm_shared::{address::Address, econ::TokenAmount};
+use serde_tuple::{Deserialize_tuple, Serialize_tuple};
+
+#[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug)]
+pub struct MintParams {
+    pub initial_owner: Address,
+    #[serde(with = "bigint_ser")]
+    pub amount: TokenAmount,
+}
 
 fn main() {
     let input: &str =
-        "goMAWBuCQwDqB1UCav1ChP2Wqo8IQXOOMCvUH9gFqoYAgkMA6gdVAmr9QoT9lqqPCEFzjjAr1B/YBaqG";
+        "goMAWBuCQwDqB1UCgfjJ3ZOCNp6QjnIs6lUnh32J0fwAgkMA6gdVAoH4yd2TgjaekI5yLOpVJ4d9idH8";
 
     let vec = base64::decode(input).unwrap();
 
     let bytes = RawBytes::from(vec);
     let params = bytes.deserialize::<CreateFrc42Return>().unwrap();
     println!("{:#?}", params);
+
+    let mint_params =
+        MintParams { initial_owner: Address::new_id(1006), amount: TokenAmount::from(1000) };
+
+    let serialized = fvm_ipld_encoding::to_vec(&mint_params).unwrap();
+    let res = base64::encode(&serialized);
+    println!("{:?}", res)
 }
